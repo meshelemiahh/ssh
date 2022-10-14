72ecd4e047150be54eb6076eec7e8c9218a59c8f
meshelemiahh <115239742+meshelemiahh@users.noreply.github.com>
2022-10-14 13:51:35 +0100

    Assignment 1.py

---
+++ Assignment 1.py
@@ -0,0 +1,31 @@
+import netmiko
+import getpass
+
+connectionInfo = {
+    'device_type' : '',
+    'host':'192.168.1.1',
+    'username':'cisco',
+    'password':'',
+    'secret':''
+}
+
+print("*** Device connection script ***")
+connectionInfo["password"] = getpass.getpass("Enter decive password: ")
+connectionInfo["secret"] = getpass.getpass("Enter device secret: ")
+connectionType = input("Do you want to connect via SSH (1) or Telnet(2)?")
+
+if connectionType == "1":
+    connectionInfo["device_type"] = "cisco_ios"
+    session = netmiko.ConnectionHandler(**connectionInfo)
+    session.enable()
+    output = session.send_command('sh run')
+    file = open("config-output- " + connectionInfo["host"] + ".txt", "w")
+    file.write(output)
+    file.close()
+elif connectionType == "2":
+    connectionInfo["device_type"] = "cisco_ios_telnet"
+    session = netmiko.ConnectionHandler(**connectionInfo)
+    session.enable()
+    print(session.send_command('sh ip int br'))
+else:
+    exit()
