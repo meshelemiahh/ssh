499f00915a5a5dbf0f656fab01148db044ba112d
meshelemiahh <115239742+meshelemiahh@users.noreply.github.com>
2022-10-21 12:22:59 +0100

    Update Assignment 1.py

--- Assignment 1.py
+++ Assignment 1.py
@@ -17,14 +17,17 @@ connectionType = input("Do you want to connect via SSH (1) or Telnet(2)?")
 if connectionType == "1":
     connectionInfo["device_type"] = "cisco_ios"
     session = netmiko.ConnectionHandler(**connectionInfo)
+    print("*** Connection Successful ***)
     session.enable()
     output = session.send_command('sh run')
     file = open("config-output- " + connectionInfo["host"] + ".txt", "w")
     file.write(output)
     file.close()
+    print("*** Config Saved ***)
 elif connectionType == "2":
     connectionInfo["device_type"] = "cisco_ios_telnet"
     session = netmiko.ConnectionHandler(**connectionInfo)
+    print("*** Connection Successful ***)
     session.enable()
     print(session.send_command('sh ip int br'))
 else:
